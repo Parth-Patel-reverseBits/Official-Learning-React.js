54a1bff7d718f1d0af73e5837604a5febdb195a3
Parth Patel <parth.patel.reversebits@gmail.com>
2025-05-15 12:05:58 +0530

    added scrollbar for rendring list

--- react-project-demos/src/quick-start/components/RendringLists.tsx
+++ react-project-demos/src/quick-start/components/RendringLists.tsx
@@ -5,7 +5,7 @@ const RendringLists = () => {
     <div className="mb-10">
       <h3 className="font-bold mt-5">Rendring Lists</h3>
       <div>
-        <ul>
+        <ul className="h-60 overflow-auto border-2 px-3 rounded-2xl">
           {products.map((item) => {
             return (
               <li>
