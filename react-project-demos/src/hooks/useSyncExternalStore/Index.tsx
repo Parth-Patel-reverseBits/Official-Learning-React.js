c20e27e46bd74b503bc23c9edd6bdefe7bf60ccd
Parth Patel <parth.patel.reversebits@gmail.com>
2025-06-09 18:47:36 +0530

    added navigation button in use-sync-externel-store component

--- react-project-demos/src/hooks/useSyncExternalStore/Index.tsx
+++ react-project-demos/src/hooks/useSyncExternalStore/Index.tsx
@@ -1,15 +1,21 @@
 import NormalGlobalState from "./components/NormalGlobalState/NormalGlobalState";
 import UseSyncExternelStoreState from "./components/useSyncExternelStoreState/UseSyncExternelStoreState";
+import NavigationButton from "../../GlobalComponents/NavigationButton";
+
 const Index = () => {
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center mt-20">
         useSyncExternelStore hook demo
       </h1>
-      <div className="flex justify-between px-30">
+      <div className="flex justify-between px-30 mt-30">
         <NormalGlobalState />
         <UseSyncExternelStoreState />
       </div>
+      <NavigationButton left="left" path="/use-optimistic">
+        Go to previous page
+      </NavigationButton>
+      <NavigationButton path="">Go to next page</NavigationButton>
     </div>
   );
 };
