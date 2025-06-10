96a697b6c3a5d87a3cfdc7c183c107d70af1c99c
Parth Patel <parth.patel.reversebits@gmail.com>
2025-06-10 11:50:01 +0530

    added react-logo animation on homepage

--- react-project-demos/src/HomePage.tsx
+++ react-project-demos/src/HomePage.tsx
@@ -1,12 +1,15 @@
 import NavigationButton from "./GlobalComponents/NavigationButton";
-
+import reactLogo from "./assets/react.svg";
+import "./index.css";
 const HomePage = () => {
   return (
     <>
       <div className="h-screen flex justify-center items-center font-bold text-4xl ">
         Welcome To React.js Hooks Demos
+        <div className="absolute top-50">
+          <img src={reactLogo} className="w-20" alt="" id="reactLogo-image" />
+        </div>
       </div>
-      <div></div>
       <NavigationButton path="/use-action-state">
         Go to next page
       </NavigationButton>
